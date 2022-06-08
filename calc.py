2a75b060108e22613a4ca5a2fa1006a6a19f6c45
Gabriel Travaini <gabrieltravaini.8039@aluno.saojudas.br>
2022-06-08 19:29:17 -0300

    issumemo

---
+++ calc.py
@@ -0,0 +1,28 @@
+import os
+from re import sub
+
+os.system('cls')
+
+
+def soma (a,b):
+    res = a+b
+    return res
+
+def subtracao (c,d):
+    res = c-d
+    return res
+
+def multi(e,f):
+    res = e.f
+    return res
+
+a=4
+b=3
+c=2
+d=1
+
+print(soma(a,b))
+
+print(subtracao(c,d))
+
+print(multi(a,d))
